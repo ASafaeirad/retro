25490d11113a493f5a3be19606cc2bad508167bc
ASafaierad <alireza.safaierad@gmail.com>
2026-05-23 22:19:38 +0200

    fix button api

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,23 +1,35 @@
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
 
-import { cn } from "#lib/cn";
+import { cn, join } from "#lib/cn";
 
 const buttonVariants = cva(
-  "inline-flex items-center justify-center whitespace-nowrap rounded-base text-sm font-base ring-offset-white transition-all gap-2 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50",
+  join(
+    "inline-flex items-center justify-center whitespace-nowrap rounded-base text-sm font-base",
+    "ring-offset-white transition-all gap-2",
+    "[&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
+    "focus-visible:outline-hidden focus-visible:ring-2 focus-visible:ring-black focus-visible:ring-offset-2",
+    "disabled:pointer-events-none disabled:opacity-50",
+  ),
   {
     variants: {
       variant: {
         default:
-          "text-main-foreground bg-main border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none cursor-pointer",
-        noShadow: "text-main-foreground bg-main border-2 border-border",
+          "text-main-foreground bg-main border-2 border-border cursor-pointer",
         neutral:
-          "bg-secondary-background text-foreground border-2 border-border shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none",
+          "bg-secondary-background text-foreground border-2 border-border ",
+        danger: "bg-danger border-2 border-border",
+      },
+      shadow: {
+        default:
+          "shadow-shadow hover:translate-x-boxShadowX hover:translate-y-boxShadowY hover:shadow-none",
         reverse:
-          "text-main-foreground bg-main border-2 border-border hover:translate-x-reverseBoxShadowX hover:translate-y-reverseBoxShadowY hover:shadow-shadow",
+          "hover:translate-x-reverseBoxShadowX hover:translate-y-reverseBoxShadowY hover:shadow-shadow",
+        none: "shadow-none",
       },
       size: {
         default: "h-10 px-4 py-2",
+        xs: "h-6 px-1 text-xs",
         sm: "h-9 px-3",
         lg: "h-11 px-8",
         icon: "size-10",
@@ -26,6 +38,7 @@ const buttonVariants = cva(
     defaultVariants: {
       variant: "default",
       size: "default",
+      shadow: "default",
     },
   },
 );
@@ -35,6 +48,7 @@ export function Button({
   variant,
   size,
   asChild = false,
+  shadow,
   ...props
 }: React.ComponentProps<"button"> &
   VariantProps<typeof buttonVariants> & {
@@ -46,7 +60,7 @@ export function Button({
     <Comp
       data-slot="button"
       type="button"
-      className={cn(buttonVariants({ variant, size, className }))}
+      className={cn(buttonVariants({ variant, size, shadow, className }))}
       {...props}
     />
   );
--- src/lib/cn.ts
+++ src/lib/cn.ts
@@ -5,3 +5,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function join(...classes: ClassValue[]) {
+  return classes.filter(Boolean).join(" ");
+}
