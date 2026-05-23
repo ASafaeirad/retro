27f823e30c64dbd006d3a2465c082a50e5389950
ASafaierad <alireza.safaierad@gmail.com>
2026-05-23 22:19:22 +0200

    add badge

---
+++ src/components/ui/badge.tsx
@@ -0,0 +1,45 @@
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "#lib/cn";
+
+const badgeVariants = cva(
+  "inline-flex items-center justify-center rounded-base border-2 border-border font-base w-fit whitespace-nowrap shrink-0 [&>svg]:size-3 gap-1 [&>svg]:pointer-events-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] overflow-hidden",
+  {
+    variants: {
+      variant: {
+        default: "bg-main text-main-foreground",
+        neutral: "bg-secondary-background text-foreground",
+      },
+      size: {
+        default: "text-xs px-2.5 py-0.5",
+        sm: "text-xs px-1.5 py-0.5",
+        xs: "text-xs px-1 py-0.5",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+export function Badge({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"span"> &
+  VariantProps<typeof badgeVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "span";
+
+  return (
+    <Comp
+      data-slot="badge"
+      className={cn(badgeVariants({ variant, size }), className)}
+      {...props}
+    />
+  );
+}
