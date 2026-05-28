154cec4d23cfa730fa443fb1bdbcf6ab5e6fef06
ASafaierad <alireza.safaierad@gmail.com>
2026-05-28 20:44:42 +0200

    add group by category

--- src/models/ticket.model.ts
+++ src/models/ticket.model.ts
@@ -9,3 +9,11 @@ export interface Ticket {
   category: TicketCategory;
   groupId?: Id<"ticketGroups">;
 }
+
+export const groupByCategory = (tickets: Ticket[]) => {
+  const grouped = Object.groupBy(tickets, (ticket) => ticket.category);
+  return {
+    wellTickets: grouped["well"] || [],
+    improveTickets: grouped["improve"] || [],
+  };
+};
